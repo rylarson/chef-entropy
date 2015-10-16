a0e54d71cdedbf1228f5f8ec90b628fb834d8104
Craig Meinschein <cmeinschein@tripwire.com>
2015-10-16 16:29:56 -0700

    Add support for SUSE 11

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ryan.mango.larson@gmail.com'
 license          'MIT'
 description      'Ensures a good source of entropy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.1.0'
 
 depends 'yum-epel', '<= 0.6.0'
 depends 'apt', '>= 0.0.0'
@@ -16,4 +16,4 @@ supports 'oracle', '>= 5.0'
 # We claim rhel support because we work on CentOS but we don't have the ability to test on rhel
 supports 'redhat', '>= 5.0'
 supports 'fedora', '>= 18.0'
-supports 'suse', '>= 12.2'
+supports 'suse', '>= 11.0'
